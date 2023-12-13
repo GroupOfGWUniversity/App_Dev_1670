ea6eae8961ceb472e4fd9be466d28c6bcfd9f997
PainnIrene <hoanghuytran10102003@gmail.com>
2023-12-14 06:28:31 +0700

    Update ApplicationDatabase.cs

--- Data/ApplicationDatabase.cs
+++ Data/ApplicationDatabase.cs
@@ -11,6 +11,10 @@ namespace App_Dev_1670.Data
     public class ApplicationDatabase : IdentityDbContext<Customer> //dùng xuyên suốt hệ thống
     {
 
+       d public DbSet<Book> Books { get; set; }
+        public DbSet<Order> OrderDetails { get; set; }
+        public DbSet<Order> OrderHeader { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
 
