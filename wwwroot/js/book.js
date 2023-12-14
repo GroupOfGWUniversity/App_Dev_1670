f7d830952ca389a05367a4ae3a5ff706f6d9ed7a
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2023-12-14 23:10:11 +0700

    test

--- wwwroot/js/book.js
+++ wwwroot/js/book.js
@@ -18,13 +18,11 @@ function loadDataTable() {
             { data: 'instock', "width": "15%" },
             { data: 'category.name', "width": "10%" },
             { data: 'seller.name', "width": "15%" },
-
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group"> <a href="/admin/book/createupdate?id=${data}" class="btn btn-primary mx-2" > <i class="bi bi-pencil-square"></i>Edit</a ><a onClick=Delete('/admin/book/delete/${data}') class="btn btn-danger mx-2" ><i class="bi bi-pencil-square"></i>Delete</a ></div >`
                 },
-
                 "width": "25%"
             }
         ]
