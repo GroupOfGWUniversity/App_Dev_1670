c91d55efcf6413776bd6ce4259afbd452a901255
PainnIrene <hoanghuytran10102003@gmail.com>
2023-12-22 14:35:26 +0700

    Update UI

--- Areas/User/Controllers/HomeController.cs
+++ Areas/User/Controllers/HomeController.cs
@@ -98,7 +98,6 @@ namespace App_Dev_1670.Areas.User.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-
         public IActionResult Privacy()
         {
             return View();
@@ -110,5 +109,77 @@ namespace App_Dev_1670.Areas.User.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         //function update data
+      
+        //public IActionResult AddtoCartFromHome(Book book)
+        //{
+        //    Cart cart = new()
+        //    {
+        //        Book = _unitOfWork.Book.Get(u => u.BookID == book.BookID, includeProperty: "Category"),
+        //        Count = 1,
+        //        BookID = book.BookID
+
+        //    };
+        //    return View(cart);
+        //}
+        //[HttpPost]
+        //[Authorize]
+        //public IActionResult AddtoCartFromHome(Cart cart)
+        //{
+        //    var claimsIdentity = (ClaimsIdentity)User.Identity;
+        //    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        //    cart.ApplicationUserID = userId;
+        //    Cart cartFromDb = _unitOfWork.Cart.Get(c => c.ApplicationUserID == userId && c.BookID == cart.BookID);
+        //    var bookToUpdate = _unitOfWork.Book.Get(u => u.BookID == cart.BookID);
+
+        //    if (cartFromDb != null)
+        //    {
+        //        // Kiểm tra xem số lượng sách trong kho có đủ để giảm đi không
+        //        if (bookToUpdate.InStock >= cart.Count)
+        //        {
+        //            bookToUpdate.InStock = Math.Max(0, bookToUpdate.InStock - cart.Count);
+
+        //            // Cập nhật thay đổi vào cơ sở dữ liệu
+        //            _unitOfWork.Book.Update(bookToUpdate);
+        //            _unitOfWork.Save();
+
+        //            // Cập nhật số lượng trong giỏ hàng
+        //            cartFromDb.Count += cart.Count;
+        //            _unitOfWork.Cart.Update(cartFromDb);
+        //        }
+        //        else
+        //        {
+        //            TempData["Error"] = "Not enough stock available.";
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // Kiểm tra xem số lượng sách trong kho có đủ để thêm vào giỏ hàng không
+        //        if (bookToUpdate.InStock >= cart.Count)
+        //        {
+        //            // Giảm số lượng sách trong kho
+        //            bookToUpdate.InStock = Math.Max(0, bookToUpdate.InStock - cart.Count);
+
+        //            // Cập nhật thay đổi vào cơ sở dữ liệu
+        //            _unitOfWork.Book.Update(bookToUpdate);
+        //            _unitOfWork.Save();
+
+        //            // Thêm mới vào giỏ hàng
+        //            _unitOfWork.Cart.Add(cart);
+        //        }
+        //        else
+        //        {
+        //            TempData["Error"] = "Not enough stock available.";
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //    }
+
+        //    TempData["Success"] = "Cart updated successfully";
+        //    int itemCount = _unitOfWork.Cart.GetAll(c => c.ApplicationUserID == userId).Sum(c => c.Count);
+        //    TempData["ItemCount"] = itemCount;
+        //    _unitOfWork.Save();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
     }
 }
